60d5dd3d71ac1415a5af2a984d8a1834e422e90b
unknown <2880FRC@gmail.com>
2020-02-10 19:06:54 -0500

    Created TestSubsystem. Began attempt to grab values from the limelight.

---
+++ src/main/java/frc/robot/subsystems/TestSubsystem.java
@@ -0,0 +1,41 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+
+public class TestSubsystem extends SubsystemBase {
+
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    NetworkTableEntry tx = table.getEntry("tx");
+    NetworkTableEntry ty = table.getEntry("ty");
+    NetworkTableEntry ta = table.getEntry("ta");
+  
+  /**
+   * Creates a new TestSubsystem.
+   */
+  public TestSubsystem() {
+     
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    double x = tx.getDouble(0.0);
+    double y = ty.getDouble(0.0);
+    double area = ta.getDouble(0.0);
+
+    Shuffleboard.getTab("Main").add("tx", x);
+    Shuffleboard.getTab("Main").add("ty", y);
+    Shuffleboard.getTab("Main").add("ta", area);
+  }
+}
