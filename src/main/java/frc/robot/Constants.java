19fb5f5ad32f2bde491b8eb6c4f977f7055d3f61
Kiersten Mitzel <kdmitzel43@yahoo.com>
2020-03-03 17:29:17 -0500

    Fixed changes in constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -18,15 +18,6 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-    public static final int BALL_PICKUP_CONTROLLER_VICTOR_SPX_ID = 3;
-    public static final int SHOOTER_CONTROLLER_LEFT_SIDE_VICTOR_SPX_ID = 2;
-    public static final int SHOOTER_CONTROLLER_RIGHT_SIDE_VICTOR_SPX_ID = 1;
-    public static final int MAGAZINE_CONTROLLER_VICTOR_SPX_ID = 4;
-
-    public static final int LEFT_FRONT_SPARK_CONTROLLER_ID = 1;
-    public static final int LEFT_BACK_SPARK_CONTROLLER_ID = 2;
-    public static final int RIGHT_FRONT_SPARK_CONTROLLER_ID = 3;
-    public static final int RIGHT_BACK_SPARK_CONTROLLER_ID = 4;
 
     public static final double TELEOP_DRIVE_SPEED_MODIFIER = 1.0;
 
@@ -37,4 +28,20 @@ public final class Constants {
     public static final int XBOX_ASSISTANT_DRIVER_CONTROLLER_ID = 1;
 
     public static final double NO_SPEED = 0.0;
+
+    public static final int LEFT_FRONT_SPARK_MAX_ID = 3;
+    public static final int LEFT_BACK_SPARK_MAX_ID = 1;
+    public static final int RIGHT_FRONT_SPARK_MAX_ID = 4;
+    public static final int RIGHT_BACK_SPARK_MAX_ID = 2;
+
+    public static final MotorType BRUSHED_MOTOR = MotorType.kBrushed;
+
+
+    public static final int XBOX_DRIVER_CONTROLLER_PORT_ID = 4;
+    public static final int DRIVER_JOYSTICK_X_PORT_ID = 1;
+    public static final int DRIVER_JOYSTICK_Y_PORT_ID = 0;
+    public static final double TELEOP_FORWARD_SPEED_MODIFIER = 0.4;
+
+    public static final double TELEOP_BACKWARDS_SPEED_MODIFIER = -0.4;
+
 }
