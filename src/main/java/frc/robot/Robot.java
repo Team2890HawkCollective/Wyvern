1a022a72e603aab120df9c8734951bd5edc41912
Kiersten Mitzel <kdmitzel43@yahoo.com>
2020-03-02 19:19:49 -0500

    Added disable of limelight light when teleop is initialized

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -126,6 +126,8 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
+
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(1);
     // This makes sure that the autonomous stops running when
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
